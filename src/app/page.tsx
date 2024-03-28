005021e1458b4d4bc98728e3937e9d518ce44706
김창아 <changagim@snu.ac.kr>
2024-03-28 15:51:30 +0900

    remove axiosAuthConfig hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import Home from '@/containers/home';
+const Home = dynamic(() => import('@/containers/home'), {
+  ssr: false,
+});
+import dynamic from 'next/dynamic';
 
 export default async function Page() {
   return <Home />;
--- src/services/post/post.ts
+++ src/services/post/post.ts
@@ -3,14 +3,12 @@ import {
   useSuspenseQuery,
 } from '@tanstack/react-query';
 import { axiosAuth } from '../axiosInstance';
-import { useAxiosConfig } from '@/hooks/useAxiosConfig';
 
 export const getPostList = (options?: UseSuspenseQueryOptions) => {
-  const config = useAxiosConfig();
   return useSuspenseQuery({
     queryKey: ['post-list'],
     queryFn: async () => {
-      const res = await axiosAuth.get('post', config);
+      const res = await axiosAuth.get('post');
       return res.data as IResponse<IPost[]>;
     },
     ...options,
